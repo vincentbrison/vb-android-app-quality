1cbf060625f25006b7a93536df1a9c3aa4d45d26
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-03-24 22:29:26 +0100

    adding JavaDoc comments

--- app/src/main/java/vb/android/app/quality/NumberGenerator.java
+++ app/src/main/java/vb/android/app/quality/NumberGenerator.java
@@ -1,6 +1,14 @@
 package vb.android.app.quality;
 
+/**
+ * Class for generating numbers
+ */
 public class NumberGenerator {
+
+    /**
+     * generates any number
+     * @return any number
+     */
     public int generateAnyNumber() {
         return 42;
     }
