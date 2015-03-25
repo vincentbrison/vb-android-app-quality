1a8e0b379e6e879b456c526e29db92f2e8437716
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-03-25 22:10:41 +0100

    adding JavaDoc and cleaning the project

--- app/src/main/java/vb/android/app/quality/Greeting.java
+++ app/src/main/java/vb/android/app/quality/Greeting.java
@@ -4,5 +4,9 @@ package vb.android.app.quality;
  * Interface for generating greeting
  */
 public interface Greeting {
+    /**
+     * generates greeting
+     * @return string with greeting
+     */
     String generate();
 }
