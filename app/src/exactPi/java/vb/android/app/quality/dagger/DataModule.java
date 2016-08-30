35bbafbdf13a280c5d05acbbc689f587d8bf88ff
Frieder Bluemle <frieder.bluemle@gmail.com>
2016-08-30 22:19:50 +0800

    Get url from resources

--- app/src/exactPi/java/vb/android/app/quality/dagger/DataModule.java
+++ app/src/exactPi/java/vb/android/app/quality/dagger/DataModule.java
@@ -16,6 +16,8 @@
 
 package vb.android.app.quality.dagger;
 
+import android.content.Context;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -25,12 +27,15 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import vb.android.app.quality.R;
 import vb.android.app.quality.rest.ApiInterface;
 
 /**
  * Module which expose data providers implementation used by the app.
  */
-@Module
+@Module(includes = {
+        AppModule.class
+})
 public final class DataModule {
     /**
      * Return the OkHttpClient implementation used by this app.
@@ -48,14 +53,15 @@ public final class DataModule {
     /**
      * Return the REST API implementation used by this app.
      *
+     * @param context is used to resolve string.
      * @param client the OkHttpClient to use.
      * @return the REST API implementation used by this app.
      */
     @Provides
     @Singleton
-    public ApiInterface provideApi(OkHttpClient client) {
+    public ApiInterface provideApi(Context context, OkHttpClient client) {
         return new Retrofit.Builder()
-                .baseUrl("http://echo.jsontest.com")
+                .baseUrl(context.getString(R.string.url))
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .client(client)
