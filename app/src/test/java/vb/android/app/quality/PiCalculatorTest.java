b4e3c9015978f7449408646b7245e7e7f44dbb15
Frieder Bluemle <frieder.bluemle@gmail.com>
2016-08-30 20:59:45 +0800

    Use BuildConfig for flavor type

--- app/src/test/java/vb/android/app/quality/PiCalculatorTest.java
+++ app/src/test/java/vb/android/app/quality/PiCalculatorTest.java
@@ -21,16 +21,18 @@ import junit.framework.TestCase;
 /**
  * Created by Vincent Brison on 14/06/2015.
  */
-public class PiTest extends TestCase {
+public class PiCalculatorTest extends TestCase {
     public void testCalcPiDigits() throws Exception {
-        if (Contants.FLAVOR.equals(Flavors.APPROXIMATION_PI)) {
+        if ("approximationPi".equals(BuildConfig.FLAVOR)) {
             double pi = new PiCalculator().calcPiDigits(500);
-            assertTrue("After 500 interations, computed Pi (" +
+            assertTrue("After 500 iterations, computed Pi (" +
                     pi +
                     ") should have at least 2 decimals rights.", pi <= 3.15 && pi >= 3.13);
         } else {
             double pi = new PiCalculator().calcPiDigits(5);
-            assertTrue("The 5 first decimals of computed Pi (" + pi + ") should be correct.", pi == 3.1415);
+            assertTrue("The 5 first decimals of computed Pi (" +
+                    pi +
+                    ") should be correct.", pi == 3.1415);
         }
     }
 }
